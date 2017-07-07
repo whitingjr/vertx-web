e9828e334c065ff14b81af9e72210e1cc282c0bf
Jeremy Whiting <jwhiting@redhat.com>
2017-07-07 08:54:24 +0100

    Changed to release the buffer.

--- vertx-web/src/main/java/io/vertx/ext/web/handler/impl/BodyHandlerImpl.java
+++ vertx-web/src/main/java/io/vertx/ext/web/handler/impl/BodyHandlerImpl.java
@@ -19,7 +19,10 @@ package io.vertx.ext.web.handler.impl;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.impl.FileUploadImpl;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.FileUpload;
@@ -36,11 +39,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class BodyHandlerImpl implements BodyHandler {
 
+  private static final Logger log = LoggerFactory.getLogger(BodyHandlerImpl.class);
+
   private static final String BODY_HANDLED = "__body-handled";
 
   private long bodyLimit = DEFAULT_BODY_LIMIT;
   private String uploadsDir;
   private boolean mergeFormAttributes = DEFAULT_MERGE_FORM_ATTRIBUTES;
+  private boolean deleteUploadedFilesOnEnd = DEFAULT_DELETE_UPLOADED_FILES_ON_END;
 
   public BodyHandlerImpl() {
     setUploadsDirectory(DEFAULT_UPLOADS_DIRECTORY);
@@ -61,6 +67,11 @@ public class BodyHandlerImpl implements BodyHandler {
       request.endHandler(v -> handler.end());
       context.put(BODY_HANDLED, true);
     } else {
+      // on reroute we need to re-merge the form params if that was desired
+      if (mergeFormAttributes && request.isExpectMultipart()) {
+        request.params().addAll(request.formAttributes());
+      }
+
       context.next();
     }
   }
@@ -83,6 +94,12 @@ public class BodyHandlerImpl implements BodyHandler {
     return this;
   }
 
+  @Override
+  public BodyHandler setDeleteUploadedFilesOnEnd(boolean deleteUploadedFilesOnEnd) {
+    this.deleteUploadedFilesOnEnd = deleteUploadedFilesOnEnd;
+    return this;
+  }
+
   private class BHandler implements Handler<Buffer> {
 
     RoutingContext context;
@@ -90,24 +107,34 @@ public class BodyHandlerImpl implements BodyHandler {
     boolean failed;
     AtomicInteger uploadCount = new AtomicInteger();
     boolean ended;
+    long uploadSize = 0L;
+
+    final boolean isMultipart;
+    final boolean isUrlEncoded;
 
     public BHandler(RoutingContext context) {
       this.context = context;
       Set<FileUpload> fileUploads = context.fileUploads();
-      makeUploadDir(context.vertx().fileSystem());
 
-      context.request().setExpectMultipart(true);
+      final String contentType = context.request().getHeader(HttpHeaders.CONTENT_TYPE);
+      isMultipart = contentType != null && contentType.contains("multipart/form-data");
+      isUrlEncoded = contentType != null && contentType.contains("application/x-www-form-urlencoded");
+
+      if (isMultipart || isUrlEncoded) {
+        makeUploadDir(context.vertx().fileSystem());
+        context.request().setExpectMultipart(true);
+        context.request().uploadHandler(upload -> {
+          // we actually upload to a file with a generated filename
+          uploadCount.incrementAndGet();
+          String uploadedFileName = new File(uploadsDir, UUID.randomUUID().toString()).getPath();
+          upload.streamToFileSystem(uploadedFileName);
+          FileUploadImpl fileUpload = new FileUploadImpl(uploadedFileName, upload);
+          fileUploads.add(fileUpload);
+          upload.exceptionHandler(context::fail);
+          upload.endHandler(v -> uploadEnded());
+        });
+      }
       context.request().exceptionHandler(context::fail);
-      context.request().uploadHandler(upload -> {
-        // We actually upload to a file with a generated filename
-        uploadCount.incrementAndGet();
-        String uploadedFileName = new File(uploadsDir, UUID.randomUUID().toString()).getPath();
-        upload.streamToFileSystem(uploadedFileName);
-        FileUploadImpl fileUpload = new FileUploadImpl(uploadedFileName, upload);
-        fileUploads.add(fileUpload);
-        upload.exceptionHandler(context::fail);
-        upload.endHandler(v -> uploadEnded());
-      });
     }
 
     private void makeUploadDir(FileSystem fileSystem) {
@@ -121,38 +148,79 @@ public class BodyHandlerImpl implements BodyHandler {
       if (failed) {
         return;
       }
-      if (bodyLimit != -1 && (body.length() + buff.length()) > bodyLimit) {
+      uploadSize += buff.length();
+      if (bodyLimit != -1 && uploadSize > bodyLimit) {
         failed = true;
         context.fail(413);
       } else {
-        body.appendBuffer(buff);
+        // multipart requests will not end up in the request body
+        // url encoded should also not, however jQuery by default
+        // post in urlencoded even if the payload is something else
+        if (!isMultipart /* && !isUrlEncoded */) {
+          body.appendBuffer(buff);
+        }
       }
     }
 
     void uploadEnded() {
       int count = uploadCount.decrementAndGet();
-      if (count == 0) {
+      // only if parsing is done and count is 0 then all files have been processed
+      if (ended && count == 0) {
         doEnd();
       }
     }
 
     void end() {
+      // this marks the end of body parsing, calling doEnd should
+      // only be possible from this moment onwards
+      ended = true;
+
+      // only if parsing is done and count is 0 then all files have been processed
       if (uploadCount.get() == 0) {
         doEnd();
       }
     }
 
     void doEnd() {
-      if (failed || ended) {
+      if (deleteUploadedFilesOnEnd) {
+        if (failed) {
+          deleteFileUploads();
+        } else {
+          context.addBodyEndHandler(x -> deleteFileUploads());
+        }
+      }
+
+      if (failed) {
+        if (body.isDirectOrPooled()){
+          body.close();
+        }
         return;
       }
-      ended = true;
+
       HttpServerRequest req = context.request();
       if (mergeFormAttributes && req.isExpectMultipart()) {
         req.params().addAll(req.formAttributes());
       }
       context.setBody(body);
       context.next();
+    }
+
+    private void deleteFileUploads() {
+      for (FileUpload fileUpload : context.fileUploads()) {
+        FileSystem fileSystem = context.vertx().fileSystem();
+        String uploadedFileName = fileUpload.uploadedFileName();
+        fileSystem.exists(uploadedFileName, existResult -> {
+          if (existResult.failed()) {
+            log.warn("Could not detect if uploaded file exists, not deleting: " + uploadedFileName, existResult.cause());
+          } else if (existResult.result()) {
+            fileSystem.delete(uploadedFileName, deleteResult -> {
+              if (deleteResult.failed()) {
+                log.warn("Delete of uploaded file failed: " + uploadedFileName, deleteResult.cause());
+              }
+            });
+          }
+        });
+      }
     }
   }
 
